8bdcfb55c2b56dac30f8aac39c73808227bbd645
Derek Cassel <derekdcassel@gmail.com>
2017-08-04 14:17:17 -0600

    more pseudo js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,19 @@
-//Sign up page, "submit profile" opens the Home Page "Task ++"
+//Sign up page, "submit profile" opens the Home Page "Task ++"(only if we have time to html)
 
-//Home Page Task++
-	//Profile parent
+//Home Page Task++ --------------------------
+	//MANAGE TASKS
+		//onclick to opens modal **with passcode?**
+			//modal contents
+				//CURRENT DATE PRINT
+				//user tabs(to select which child to add task to)
+				//add new user
+				//table linked to firebase(to append tasks)
+				//add task +(onclick to show add task form with apply button)
+					//apply appends table with new task and adds task to user profile through firebase)
+				//  close button onclick (will close the modal)
+				
+
 
 	//profile child 
+		//firebase link to add tasks from parent modal to child card div
+		//including check box that displays congrats gif
